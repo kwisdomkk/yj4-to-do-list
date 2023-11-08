532d17e056c0346e1edb39ad8c66ba02897042e2
kwisdomkk <kwisdom@naver.com>
2023-11-08 13:19:43 +0900

    javascript variables

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello js")
+// 변수
+const whoAmI = "권지혜"
+let hobby = "달리기"
+
+const a = "10"
+const b = 20
+
+console.log(typeof whoAmI)
+console.log(typeof a)
+
+// " ' 
+// ' '
+// ` `
+
+
+// console.log("나의 이름은" + whoAmI + "이고, 나의 취미는 " + hobby + 입니다.")
+// console.log(`나의 이름은 ${whoAmI}이고, 나의 취미는 ${hobby}입니다.`)
+
+const arr = [1, 2, 3]
+console.log(arr[0])
